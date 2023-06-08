6109e0199f85a42f9ff8ccee47ce3fcbb4dc9ea2
doryosi <103187334+doryosi@users.noreply.github.com>
2023-06-08 21:36:01 +0300

    Update run_me.sh

--- run_me.sh
+++ run_me.sh
@@ -8,6 +8,13 @@ sudo apt install docker.io docker-compose
 
 sudo systemctl start docker.service
 
+# Enter Creds for Jenkins Server
+
+read -p "Choose Username for Jenkins Server: " USERNAME
+read -p "Choose Password for Jenkins Server: " PASSWORD
+export USERNAME
+export PASSWORD
+
 # Provison the Jenkins server
 
 sudo docker-compose up -d
@@ -16,3 +23,4 @@ sudo docker-compose up -d
 echo "##############################################"
 echo "Open the browser and connect to 127.0.0.1:8080"
 echo "##############################################"
+
